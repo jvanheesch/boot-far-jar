504b334e024e5de95038fe077b5cc593d66a9b01
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-11-18 12:59:09 +0100

    Notes.

--- src/main/java/com/github/jvanheesch/fat/Filter1.java
+++ src/main/java/com/github/jvanheesch/fat/Filter1.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 public class Filter1 implements Filter {
     private static final Logger LOGGER = LogManager.getLogger();
 
+    /**
+     * The issue is the following:
+     * The call to http://localhost:8080/someServlet works fine (try curl and see logs).
+     * The browser, however, also sends a favicon request.
+     * This request is handled by DispatcherServlet, after this filter writes a partial response, which leads to the exception.
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         LOGGER.info("Filter1 - start.");
