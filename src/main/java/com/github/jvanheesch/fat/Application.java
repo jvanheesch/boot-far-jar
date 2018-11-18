589d7728b8828e26a8a253e1cf4916ec028779e8
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-11-18 12:25:54 +0100

    Issue - 500.

--- src/main/java/com/github/jvanheesch/fat/Application.java
+++ src/main/java/com/github/jvanheesch/fat/Application.java
@@ -2,12 +2,22 @@ package com.github.jvanheesch.fat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter1> filter1() {
+        FilterRegistrationBean<Filter1> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new Filter1());
+        registration.addUrlPatterns("/*");
+        return registration;
     }
 }
