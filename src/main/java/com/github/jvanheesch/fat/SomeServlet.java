78e29fce90a77b1d4d9ff4d7c981ea882d335967
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-11-18 11:51:22 +0100

    boot-far-jar init commit

---
+++ src/main/java/com/github/jvanheesch/fat/SomeServlet.java
@@ -0,0 +1,23 @@
+package com.github.jvanheesch.fat;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SomeServlet", urlPatterns = "/someServlet", asyncSupported = true)
+public class SomeServlet extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        LOGGER.info("SomeServlet - start.");
+        response.getWriter().write("SomeServlet - start. \n");
+        response.getWriter().write("SomeServlet - end. \n");
+        LOGGER.info("SomeServlet - end.");
+    }
+}
